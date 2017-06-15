d906d798d456c3e04d0858639ad1a966f0ffd1ed
devjanaprime <dev@primeacademy.io>
2017-06-14 21:17:02 -0500

    part3

--- public/scripts/giphyExample.js
+++ public/scripts/giphyExample.js
@@ -6,7 +6,11 @@ myApp.controller( 'GiphyController', function( GiphyService ){
   vm.getRandom = function(){
     console.log( 'in getRandom' );
     // call GiphyService.gimmeARandom
-    GiphyService.gimmeARandom();
-    // display output on DOM
+    GiphyService.gimmeARandom().then( function(){
+      console.log( 'back in controller' );
+      vm.theGoodStuff = GiphyService.dataWeWant;
+      console.log( 'data from Service:', vm.theGoodStuff );
+      // display output on DOM
+    }); // end service function call
   }; // end getRandom
 }); // end GiphyController
