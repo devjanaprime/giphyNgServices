124aa373e8ed6421bc6843eec0673d3c7247fb4c
devjanaprime <dev@primeacademy.io>
2017-06-14 20:24:29 -0500

    nothing 2 c here

--- public/scripts/giphyExample.js
+++ public/scripts/giphyExample.js
@@ -1,5 +1,11 @@
 var myApp = angular.module( 'myApp', [] );
 
 myApp.controller( 'GiphyController', function(){
-  console.log( 'NG' );
+  var vm = this;
+
+  vm.getRandom = function(){
+    console.log( 'in getRandom' );
+    // get a random image from GiphyService.gimmeARandom
+    // display the random image on DOM
+  }; // end getRandom
 }); // end GiphyController
