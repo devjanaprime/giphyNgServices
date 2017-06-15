085f72fc61493784039aef1044dc075de909ffc9
devjanaprime <dev@primeacademy.io>
2017-06-14 20:37:07 -0500

    part 2

--- public/scripts/giphyExample.js
+++ public/scripts/giphyExample.js
@@ -1,5 +1,12 @@
 var myApp = angular.module( 'myApp', [] );
 
-myApp.controller( 'GiphyController', function(){
-  console.log( 'NG' );
+myApp.controller( 'GiphyController', function( GiphyService ){
+  var vm = this;
+
+  vm.getRandom = function(){
+    console.log( 'in getRandom' );
+    // call GiphyService.gimmeARandom
+    GiphyService.gimmeARandom();
+    // display output on DOM
+  }; // end getRandom
 }); // end GiphyController
---
+++ public/scripts/services/giphyService.js
@@ -0,0 +1,7 @@
+myApp.service( 'GiphyService', function(){
+  var sv = this;
+
+  sv.gimmeARandom = function(){
+    console.log( 'in gimmeARandom' );
+  }; //end gimmeARandom
+});
